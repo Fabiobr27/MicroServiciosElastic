a52a302956d64b980087f05b4fd230e5a4e7a3dd
Fabio Benítez Ramírez <fabio2000br@gmail.com>
2021-11-16 10:23:04 +0100

    Add files via upload

---
+++ src/main/java/com/example/microServiciosElastic/MicroserviciosElasticApplication.java
@@ -0,0 +1,13 @@
+package com.example.microServiciosElastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosElasticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microServiciosElastic/MicroserviciosElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microServiciosElastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosElasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
